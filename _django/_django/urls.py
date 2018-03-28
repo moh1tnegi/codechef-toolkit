71071666df4964ac9ddc8ae03cf973bfcaa9026e
Mohit Negi <mohitnegi165@gmail.com>
2018-03-28 11:53:00 -0700

    added assets

--- _django/_django/urls.py
+++ _django/_django/urls.py
@@ -1,7 +1,11 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.conf.urls import url
+from . import views
 
+app_name = 'codechef'
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('codechef.urls'))
+    url(r'^$', views.index, name='index'),
+    url(r'^search/$', views.search, name='search'),
+    url(r'^city-details/$', views.city_details, name='all_cities'),
+    url(r'^state-details/$', views.state_details, name='all_states'),
+    url(r'^institute-details/$', views.inst_details, name='all_inst'),
 ]
