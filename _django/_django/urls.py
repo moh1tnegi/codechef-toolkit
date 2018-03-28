7d443b1b32e5c5124a9d2e43f78ddd171593031b
mohit <mohitnegi165@gmail.com>
2018-03-29 00:31:45 +0530

    more updates <3

--- _django/_django/urls.py
+++ _django/_django/urls.py
@@ -1,11 +1,7 @@
-from django.conf.urls import url
-from . import views
+from django.contrib import admin
+from django.urls import path, include
 
-app_name = 'codechef'
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
-    url(r'^search/$', views.search, name='search'),
-    url(r'^city-details/$', views.city_details, name='all_cities'),
-    url(r'^state-details/$', views.state_details, name='all_states'),
-    url(r'^institute-details/$', views.inst_details, name='all_inst'),
+    path('admin/', admin.site.urls),
+    path('', include('codechef.urls'))
 ]
