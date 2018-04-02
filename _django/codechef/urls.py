be1badc3e1e65bab28119bb7d87fca58506f1206
Mohit Negi <mohitnegi165@gmail.com>
2018-04-02 10:58:48 -0700

    minor updates

--- _django/codechef/urls.py
+++ _django/codechef/urls.py
@@ -5,7 +5,7 @@ app_name = 'codechef'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^search/$', views.search, name='search'),
-    url(r'^city-details/$', views.city_details, name='all_cities'),
-    url(r'^state-details/$', views.state_details, name='all_states'),
-    url(r'^institute-details/$', views.inst_details, name='all_inst'),
+    url(r'^city-details/$', views.top_details, name='all_cities'),
+    url(r'^state-details/$', views.top_details, name='all_states'),
+    url(r'^institute-details/$', views.top_details, name='all_inst'),
 ]
--- _django/codechef/views.py
+++ _django/codechef/views.py
@@ -7,8 +7,8 @@ from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.core.paginator import Paginator, EmptyPage
 from django.core.paginator import PageNotAnInteger, InvalidPage
-import logging
 from .models import User
+import logging
 
 
 # LOG_FORMAT = "%()s"
@@ -16,18 +16,25 @@ logging.basicConfig(filename='log.txt',
                     level=logging.DEBUG,
                     filemode='w')
 logger = logging.getLogger()
-logger.debug("# logging starts")
+logger.debug("# logging start")
 
 top_rated = User.objects.all().order_by('-ratings')
 cities = []
 states = []
 ins = []
-counter = 1
+city_counter = 1
+stat_counter = 1
+inst_counter = 1
 for info in top_rated:
-    cities.append(str(counter)+'. '+info.city)
-    states.append(str(counter)+'. '+info.state)
-    ins.append(str(counter)+'. '+info.inst)
-    counter += 1
+    if info.city != 'NULL':
+        cities.append(str(city_counter)+'. '+info.city)
+        city_counter += 1
+    if info.state != 'NULL':
+        states.append(str(stat_counter)+'. '+info.state)
+        stat_counter += 1
+    if info.inst != 'NULL':
+        ins.append(str(inst_counter)+'. '+info.inst)
+        inst_counter += 1
 
 
 def index(request):
@@ -36,23 +43,9 @@ def index(request):
     return render(request, 'codechef/index.html', context)
 
 
-def search(request):
-    if request.method == 'GET':
-        srh = request.GET['srch']
-        if srh:
-            try:
-                match = User.objects.get(Q(username__exact=srh))
-                return render(request, 'codechef/search.html', {'sr': match})
-            except ObjectDoesNotExist:
-                messages.error(request, 'No results!')
-        else:
-            return HttpResponseRedirect('codechef/index.html')
-    return render(request, 'codechef/search.html', {'sr': 0})
-
-
 def get_details(request, toppers):
     paginator = Paginator(toppers, 10)
-    page = request.GET.get('page')
+    page = request.GET.get('page', 1)
     try:
         top_paged = paginator.get_page(page)
     except PageNotAnInteger:
@@ -63,29 +56,59 @@ def get_details(request, toppers):
     yield top_paged
 
 
-def pagination_problem(paged, paginated):
+def get_page_range(paged, paginated):
     indx = paginated.number-1
     mx_indx = len(paged.page_range)
-    logger.debug(indx)
     start = indx - 3 if indx >= 3 else 0
     end = indx + 3 if indx <= mx_indx-3 else mx_indx
-    page_range = list(paged.page_range)[start:end]
-    return page_range
+    return list(paged.page_range)[start:end]
 
 
-def city_details(request):
-    city_paginator, city_paged = get_details(request, cities)
-    page_range = pagination_problem(city_paginator, city_paged)
-    return render(request, 'codechef/details.html', {'cit': city_paged, 'page_range': page_range})
+def search(request):
+    if request.method == 'GET':
+        srh = request.GET['srch']
+        select_input = request.GET['query']
+        requested_for = 'Usernames from ' + srh
+        if select_input in 'City':
+            match = User.objects.all().filter(city__exact=srh).order_by('-ratings')
+        elif select_input in 'State':
+            match = User.objects.all().filter(state__exact=srh).order_by('-ratings')
+        elif select_input in 'Institution':
+            match = User.objects.all().filter(inst__exact=srh).order_by('-ratings')
+            logger.debug(User.objects.all().filter(inst='IHMS Kotdwar'))
+        if select_input not in 'Username':
+            details_paginator, paged = get_details(request, match)
+            page_range = get_page_range(details_paginator, paged)
+            return render(request, 'codechef/details.html',
+                            {'detail': paged, 'page_range': page_range,
+                            'requested_for': requested_for})
+        if srh:
+            try:
+                match = User.objects.get(Q(username__exact=srh))
+                return render(request, 'codechef/search.html', {'sr': match})
+            except ObjectDoesNotExist:
+                error_message = "No results found for "+srh+'. '+"""Try 
+                    checking for mispelled words. The user might be banned
+                    or not registered."""
+                messages.error(request, error_message)
+        else:
+            return HttpResponseRedirect('codechef/index.html')
+    return render(request, 'codechef/search.html', {'sr': 0})
 
 
-def state_details(request):
-    stat_paginator, state_paged = get_details(request, states)
-    page_range = pagination_problem(stat_paginator, state_paged)
-    return render(request, 'codechef/details.html', {'stat': state_paged, 'page_range': page_range})
-
-
-def inst_details(request):
-    inst_paginator, inst_paged = get_details(request, ins)
-    page_range = pagination_problem(inst_paginator, inst_paged)
-    return render(request, 'codechef/details.html', {'inst': inst_paged, 'page_range': page_range})
+def top_details(request):
+    requested_content = request.path_info
+    if requested_content == '/city-details/':
+        requested_content = cities
+        requested_for = 'Cities'
+    elif requested_content == '/state-details/':
+        requested_content = states
+        requested_for = 'States'
+    else:
+        requested_content = ins
+        requested_for = 'Institution'
+    details_paginator, paged = get_details(request, requested_content)
+    page_range = get_page_range(details_paginator, paged)
+    return render(request, 'codechef/details.html',
+                  {'detail': paged, 'page_range': page_range,
+                   'requested_for': requested_for})
