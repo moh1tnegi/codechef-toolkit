79ca2b9755c0d4ab1f8928a50e7220f60c82e703
Mohit Negi <mohitnegi165@gmail.com>
2018-04-08 10:30:19 -0700

    minor updates

--- _django/codechef/views.py
+++ _django/codechef/views.py
@@ -22,16 +22,20 @@ top_rated = User.objects.all().order_by('-ratings')
 cities = []
 states = []
 ins = []
+
 city_counter = 1
 stat_counter = 1
 inst_counter = 1
+
 for info in top_rated:
     if info.city != 'NULL':
         cities.append(str(city_counter)+'. '+info.city)
         city_counter += 1
+
     if info.state != 'NULL':
         states.append(str(stat_counter)+'. '+info.state)
         stat_counter += 1
+
     if info.inst != 'NULL':
         ins.append(str(inst_counter)+'. '+info.inst)
         inst_counter += 1
@@ -46,12 +50,14 @@ def index(request):
 def get_details(request, toppers):
     paginator = Paginator(toppers, 10)
     page = request.GET.get('page', 1)
+
     try:
         top_paged = paginator.get_page(page)
     except PageNotAnInteger:
         top_paged = paginator.get_page(1)
     except (EmptyPage, InvalidPage):
         top_paged = paginator.get_page(paginator.num_pages)
+
     yield paginator
     yield top_paged
 
@@ -59,8 +65,10 @@ def get_details(request, toppers):
 def get_page_range(paged, paginated):
     indx = paginated.number-1
     mx_indx = len(paged.page_range)
+
     start = indx - 3 if indx >= 3 else 0
     end = indx + 3 if indx <= mx_indx-3 else mx_indx
+
     return list(paged.page_range)[start:end]
 
 
@@ -69,13 +77,13 @@ def search(request):
         srh = request.GET['srch']
         select_input = request.GET['query']
         requested_for = 'Usernames from ' + srh
+
         if select_input in 'City':
             match = User.objects.all().filter(city__exact=srh).order_by('-ratings')
         elif select_input in 'State':
             match = User.objects.all().filter(state__exact=srh).order_by('-ratings')
         elif select_input in 'Institution':
             match = User.objects.all().filter(inst__exact=srh).order_by('-ratings')
-            logger.debug(User.objects.all().filter(inst='IHMS Kotdwar'))
         if select_input not in 'Username':
             details_paginator, paged = get_details(request, match)
             page_range = get_page_range(details_paginator, paged)
@@ -93,11 +101,13 @@ def search(request):
                 messages.error(request, error_message)
         else:
             return HttpResponseRedirect('codechef/index.html')
+
     return render(request, 'codechef/search.html', {'sr': 0})
 
 
 def top_details(request):
     requested_content = request.path_info
+
     if requested_content == '/city-details/':
         requested_content = cities
         requested_for = 'Cities'
@@ -107,8 +117,10 @@ def top_details(request):
     else:
         requested_content = ins
         requested_for = 'Institution'
+
     details_paginator, paged = get_details(request, requested_content)
     page_range = get_page_range(details_paginator, paged)
+
     return render(request, 'codechef/details.html',
                   {'detail': paged, 'page_range': page_range,
                    'requested_for': requested_for})
